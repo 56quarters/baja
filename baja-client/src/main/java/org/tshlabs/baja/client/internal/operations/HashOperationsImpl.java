c84561aa9432762c05a0d593dc7e5b0282b88a1c
Nick Pillitteri <nick@tshlabs.org>
2015-06-02 17:15:21 -0400

    Hash operations

--- baja-client/src/main/java/org/tshlabs/baja/client/internal/operations/HashOperationsImpl.java
+++ baja-client/src/main/java/org/tshlabs/baja/client/internal/operations/HashOperationsImpl.java
@@ -1,7 +1,80 @@
 package org.tshlabs.baja.client.internal.operations;
 
+import org.tshlabs.baja.client.operations.HashOperations;
+
+import java.util.List;
+
 /**
  *
  */
-public class HashOperationsImpl {
+public class HashOperationsImpl implements HashOperations {
+    @Override
+    public long hdel(String key, String field, String... fields) {
+        return 0;
+    }
+
+    @Override
+    public long hexists(String key, String field) {
+        return 0;
+    }
+
+    @Override
+    public String hget(String key, String field) {
+        return null;
+    }
+
+    @Override
+    public List<String> hgetall(String key) {
+        return null;
+    }
+
+    @Override
+    public long hincrby(String key, String field, long increment) {
+        return 0;
+    }
+
+    @Override
+    public String hincrbyfloat(String key, String field, float increment) {
+        return null;
+    }
+
+    @Override
+    public List<String> hkeys(String key) {
+        return null;
+    }
+
+    @Override
+    public long hlen(String key) {
+        return 0;
+    }
+
+    @Override
+    public List<String> hmget(String key, String field, String... fields) {
+        return null;
+    }
+
+    @Override
+    public String hmset(String key, String field, String value, String... others) {
+        return null;
+    }
+
+    @Override
+    public long hset(String key, String field, String value) {
+        return 0;
+    }
+
+    @Override
+    public long hsetnx(String key, String field, String value) {
+        return 0;
+    }
+
+    @Override
+    public long hstrlen(String key, String field) {
+        return 0;
+    }
+
+    @Override
+    public List<String> hvals(String key) {
+        return null;
+    }
 }
--- baja-client/src/main/java/org/tshlabs/baja/client/operations/HashOperations.java
+++ baja-client/src/main/java/org/tshlabs/baja/client/operations/HashOperations.java
@@ -1,7 +1,38 @@
 package org.tshlabs.baja.client.operations;
 
+import java.util.List;
+
 /**
  *
  */
 public interface HashOperations {
+
+    long hdel(String key, String field, String... fields);
+
+    long hexists(String key, String field);
+
+    String hget(String key, String field);
+
+    List<String> hgetall(String key);
+
+    long hincrby(String key, String field, long increment);
+
+    String hincrbyfloat(String key, String field, float increment);
+
+    List<String> hkeys(String key);
+
+    long hlen(String key);
+
+    List<String> hmget(String key, String field, String... fields);
+
+    String hmset(String key, String field, String value, String... others);
+
+    long hset(String key, String field, String value);
+
+    long hsetnx(String key, String field, String value);
+
+    long hstrlen(String key, String field);
+
+    List<String> hvals(String key);
+
 }
