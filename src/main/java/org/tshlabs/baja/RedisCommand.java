e13738f977693f1102214f714b330cd67e7f4093
Nick Pillitteri <nick@tshlabs.org>
2015-07-13 22:44:22 -0400

    Add docs for some aspects of RedisCommand

--- src/main/java/org/tshlabs/baja/RedisCommand.java
+++ src/main/java/org/tshlabs/baja/RedisCommand.java
@@ -1,11 +1,13 @@
 package org.tshlabs.baja;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 /**
- *
+ * Class for assembling a sequence arguments to send to a Redis server
+ * and read the results using a {@link RedisConnection} instance.
  */
 public class RedisCommand {
 
@@ -49,30 +51,97 @@ public class RedisCommand {
         return this;
     }
 
-    public ExecutedRedisCommand query(RedisConnection conn) {
-        conn.writeCommand(new ArrayList<>(args));
-        return new ExecutedRedisCommand(conn);
+    /**
+     * Get an immutable view of the arguments comprising this Redis command.
+     *
+     * @return Arguments that will passed to the Redis connection
+     */
+    public List<String> getArgs() {
+        return Collections.unmodifiableList(args);
     }
 
+    /**
+     * Use the given {@link RedisConnection} to send a command to the Redis
+     * server and return a facade for reading the results of the command.
+     *
+     * @param conn Connection to use for sending commands to the Redis server
+     * @return Wrapper for reading results of the command executed.
+     */
+    public ExecutedRedisCommand query(RedisConnection conn) {
+        return new ExecutedRedisCommand(conn, this);
+    }
+
+    /**
+     * Class that represents the result of executing a previously constructed
+     * {@link RedisCommand} instance using a {@link RedisConnection} instance.
+     */
     public static class ExecutedRedisCommand {
         private final RedisConnection connection;
 
-        private ExecutedRedisCommand(RedisConnection connection) {
+        private ExecutedRedisCommand(RedisConnection connection, RedisCommand cmd) {
             this.connection = Objects.requireNonNull(connection);
+            this.connection.writeCommand(Objects.requireNonNull(cmd.getArgs()));
         }
 
+        /**
+         * Get the results of the executed command as a Java {@code String}, which
+         * may be a "simple string" or "bulk string" on the Redis Server side.
+         *
+         * @return Command results as a string
+         * @throws org.tshlabs.baja.exceptions.BajaTypeMismatchException  If the result of the command
+         *                                                                was not a string type
+         * @throws org.tshlabs.baja.exceptions.BajaResourceException      If there was an I/O error
+         * @throws org.tshlabs.baja.exceptions.BajaProtocolErrorException If the Redis server responded
+         *                                                                with an error
+         */
         public String asString() {
             return connection.readSimpleOrBulkString();
         }
 
+        /**
+         * Get the results of the executed command as a Java {@code long}, which
+         * corresponds to the "integer" type on the Redis Server side.
+         *
+         * @return Command results as a long
+         * @throws org.tshlabs.baja.exceptions.BajaTypeMismatchException  If the result of the command
+         *                                                                was not a long type
+         * @throws org.tshlabs.baja.exceptions.BajaResourceException      If there was an I/O error
+         * @throws org.tshlabs.baja.exceptions.BajaProtocolErrorException If the Redis server responded
+         *                                                                with an error
+         */
         public long asLong() {
             return connection.readLong();
         }
 
+        /**
+         * Get the results of the executed command as a {@code List} of objects.
+         * <p>
+         * The actual types of the objects is undefined and should be known by the caller.
+         *
+         * @return Command results as a list
+         * @throws org.tshlabs.baja.exceptions.BajaTypeMismatchException  If the result of the command
+         *                                                                was not an array type
+         * @throws org.tshlabs.baja.exceptions.BajaResourceException      If there was an I/O error
+         * @throws org.tshlabs.baja.exceptions.BajaProtocolErrorException If the Redis server responded
+         *                                                                with an error
+         */
         public List<Object> asArray() {
             return connection.readArray();
         }
 
+        /**
+         * Get the results of the executed command as a {@code List} of {@code String}s.
+         * <p>
+         * Each entry in the list of results is converted to a {@code String} from whatever
+         * its original type was using {@code String#valueOf}. Null values will be preserved.
+         *
+         * @return Command results as a list of strings
+         * @throws org.tshlabs.baja.exceptions.BajaTypeMismatchException  If the result of the command
+         *                                                                was not an array type
+         * @throws org.tshlabs.baja.exceptions.BajaResourceException      If there was an I/O error
+         * @throws org.tshlabs.baja.exceptions.BajaProtocolErrorException If the Redis server responded
+         *                                                                with an error
+         */
         public List<String> asStringArray() {
             return connection.readStringArray();
         }
