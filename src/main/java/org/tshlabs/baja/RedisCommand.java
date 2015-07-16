17c15d10a3badf178768a3211090b84996f5dc0e
Nick Pillitteri <nick@tshlabs.org>
2015-07-15 21:00:36 -0400

    Docs for RedisCommand methods

--- src/main/java/org/tshlabs/baja/RedisCommand.java
+++ src/main/java/org/tshlabs/baja/RedisCommand.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * Class for assembling a sequence arguments to send to a Redis server
+ * Class for assembling a command and sequence arguments to send to a Redis server
  * and read the results using a {@link RedisConnection} instance.
+ *
+ * This class is <em>not</em> thread safe.
  */
 public class RedisCommand {
 
@@ -17,35 +19,79 @@ public class RedisCommand {
         this.args.add(Objects.requireNonNull(command));
     }
 
+    /**
+     * Create a new {@link RedisCommand} instance for building a command
+     * and arguments to send to the Redis server.
+     *
+     * @param command Redis command that we are executing
+     * @return Builder for sending a command and arguments to the Redis server
+     * @see <a href="http://redis.io/commands">Redis commands</a>
+     */
     public static RedisCommand cmd(String command) {
         return new RedisCommand(command);
     }
 
+    /**
+     * Append a {@code String} argument to the base Redis command.
+     *
+     * @param arg Argument to append to the command
+     * @return fluent interface
+     */
     public RedisCommand arg(String arg) {
         this.args.add(arg);
         return this;
     }
 
+    /**
+     * Append a {@code long} argument to the base Redis command.
+     *
+     * @param arg Argument to append to the command
+     * @return fluent interface
+     */
     public RedisCommand arg(long arg) {
         this.args.add(String.valueOf(arg));
         return this;
     }
 
+    /**
+     * Append an {@code int} argument to the base Redis command.
+     *
+     * @param arg Argument to append to the command
+     * @return fluent interface
+     */
     public RedisCommand arg(int arg) {
         this.args.add(String.valueOf(arg));
         return this;
     }
 
+    /**
+     * Append a {@code boolean} argument to the base Redis command.
+     *
+     * @param arg Argument to append to the command
+     * @return fluent interface
+     */
     public RedisCommand arg(boolean arg) {
         this.args.add(arg ? "1" : "0");
         return this;
     }
 
+    /**
+     * Append a {@code float} argument to the base Redis command.
+     *
+     * @param arg Argument to append to the command
+     * @return fluent interface
+     */
     public RedisCommand arg(float arg) {
         this.args.add(String.valueOf(arg));
         return this;
     }
 
+    /**
+     * Append a {@code double} argument to the base Redis command.
+     *
+     * @param arg Argument to append to the command
+     * @return fluent interface
+     */
     public RedisCommand arg(double arg) {
         this.args.add(String.valueOf(arg));
         return this;
