7c48a96674a3b80f8ed2cba2a39301139803cc9f
Nick Pillitteri <nick@tshlabs.org>
2015-08-19 16:28:14 -0400

    Use builtin method for creating ad-hoc lists

--- src/test/java/org/tshlabs/baja/RedisCommandTest.java
+++ src/test/java/org/tshlabs/baja/RedisCommandTest.java
@@ -2,9 +2,7 @@ package org.tshlabs.baja;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.eq;
@@ -14,46 +12,40 @@ import static org.mockito.Mockito.verify;
 
 public class RedisCommandTest {
 
-    private static <T> List<T> newList(T... things) {
-        final List<T> out = new ArrayList<>();
-        Collections.addAll(out, things);
-        return out;
-    }
-
     @Test
     public void testSingleArg() {
         final RedisCommand cmd = RedisCommand.cmd("INFO");
-        assertEquals(newList("INFO"), cmd.getArgs());
+        assertEquals(Arrays.asList("INFO"), cmd.getArgs());
     }
 
     @Test
     public void testArgString() {
         final RedisCommand cmd = RedisCommand.cmd("GET").arg("foo");
-        assertEquals(newList("GET", "foo"), cmd.getArgs());
+        assertEquals(Arrays.asList("GET", "foo"), cmd.getArgs());
     }
 
     @Test
     public void testArgLong() {
         final RedisCommand cmd = RedisCommand.cmd("SETEX").arg("foo").arg(123L);
-        assertEquals(newList("SETEX", "foo", "123"), cmd.getArgs());
+        assertEquals(Arrays.asList("SETEX", "foo", "123"), cmd.getArgs());
     }
 
     @Test
     public void testArgInt() {
         final RedisCommand cmd = RedisCommand.cmd("SETEX").arg("foo").arg(123);
-        assertEquals(newList("SETEX", "foo", "123"), cmd.getArgs());
+        assertEquals(Arrays.asList("SETEX", "foo", "123"), cmd.getArgs());
     }
 
     @Test
     public void testArgFloat() {
         final RedisCommand cmd = RedisCommand.cmd("INCRBYFLOAT").arg("foo").arg(3.14);
-        assertEquals(newList("INCRBYFLOAT", "foo", "3.14"), cmd.getArgs());
+        assertEquals(Arrays.asList("INCRBYFLOAT", "foo", "3.14"), cmd.getArgs());
     }
 
     @Test
     public void testArgDouble() {
         final RedisCommand cmd = RedisCommand.cmd("INCRBYFLOAT").arg("foo").arg(3.14D);
-        assertEquals(newList("INCRBYFLOAT", "foo", "3.14"), cmd.getArgs());
+        assertEquals(Arrays.asList("INCRBYFLOAT", "foo", "3.14"), cmd.getArgs());
     }
 
     @Test
@@ -64,6 +56,8 @@ public class RedisCommandTest {
                 .arg(123)
                 .query(conn);
 
-        verify(conn).writeCommand(eq(newList("SET", "foo", "123")));
+        verify(conn).writeCommand(eq(Arrays.asList("SET", "foo", "123")));
     }
+
+
 }
